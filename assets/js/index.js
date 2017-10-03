9577e6ffbff08885825a930d4b702ee1c6631882
Integ <7777js@gmail.com>
2017-10-03 12:25:53 +0800

    add animate bg

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,49 @@
 (function ($, undefined) {
     "use strict";
 
+    function scaleAnimationContainer() {
+
+        var height = $(window).height() + 5;
+        var unitHeight = parseInt(height) + 'px';
+        $('.homepage-banner').css('height', unitHeight);
+
+    }
+
+    function initBannerAnimationSize(element) {
+
+        $(element).each(function () {
+            $(this).data('height', $(this).height());
+            $(this).data('width', $(this).width());
+        });
+
+        scaleBannerAnimationSize(element);
+
+    }
+
+    function scaleBannerAnimationSize(element) {
+
+        var windowWidth = $(window).width(),
+            windowHeight = $(window).height() + 5,
+            animationWidth,
+            animationHeight;
+
+        console.log(windowHeight);
+
+        $(element).each(function () {
+            var animationAspectRatio = $(this).data('height') / $(this).data('width');
+
+            $(this).width(windowWidth);
+
+            animationHeight = windowHeight;
+            animationWidth = animationHeight / animationAspectRatio;
+            $(this).css({ 'margin-top': 0, 'margin-left': -(animationWidth - windowWidth) / 2 + 'px' });
+
+            $(this).width(animationWidth).height(animationHeight);
+
+            $('.homepage-banner .animation-container video').addClass('fadeIn animated');
+
+        });
+    }
     var $document = $(document);
 
     $document.ready(function () {
@@ -15,11 +58,23 @@
 
         $(".scroll-down").arctic_scroll();
 
-        $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
+        $(".menu-button, .nav-cover, .nav-close").on("click", function (e) {
             e.preventDefault();
             $("body").toggleClass("nav-opened nav-closed");
         });
 
+        scaleAnimationContainer();
+
+        initBannerAnimationSize('.animation-container .animation img');
+        // initBannerAnimationSize('.animation-container .mask');
+        initBannerAnimationSize('.animation-container video');
+
+        $(window).on('resize', function () {
+            scaleAnimationContainer();
+            scaleBannerAnimationSize('.animation-container .animation img');
+            // scaleBannerAnimationSize('.animation-container .mask');
+            scaleBannerAnimationSize('.animation-container video');
+        });
     });
 
     // Arctic Scroll by Paul Adam Davis
@@ -31,7 +86,7 @@
             speed: 500
         },
 
-        allOptions = $.extend(defaults, options);
+            allOptions = $.extend(defaults, options);
 
         allOptions.elem.click(function (event) {
             event.preventDefault();
@@ -43,12 +98,12 @@
 
             if (offset) {
                 toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
+                $htmlBody.stop(true, false).animate({ scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
             } else if (position) {
                 toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
+                $htmlBody.stop(true, false).animate({ scrollTop: toMove }, allOptions.speed);
             } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
+                $htmlBody.stop(true, false).animate({ scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
             }
         });
 
